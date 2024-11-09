09fb0b0b911103158ae6df5a9ffb3b2d4c532b8a
dalinxt <proxydub.1@gmail.com>
2024-11-09 15:34:54 +0530

    Moved the starter related component code to starter folder

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { workflowRegistrationPageUIURL } from "./features/workflowRegistration/endpoints";
 import { WorkflowRegistrationComponent } from "./features/workflowRegistration/WorkflowRegistrationComponent";
+import { starterPageUIURL } from "./features/starterPage/endpoints";
+import { StarterPageComponent } from "./features/starterPage/StarterPageComponent";
 
 export const UIRoutes = () => (
   <BrowserRouter>
     <Routes>
+      <Route path={starterPageUIURL} Component={StarterPageComponent} />
       <Route path={workflowRegistrationPageUIURL} Component={WorkflowRegistrationComponent} />
     </Routes>
   </BrowserRouter>
